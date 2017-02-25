4cd0c6b3622d2dc4c9e17e40be453a92a55d49f3
Electrovirus <E.electrovirus@gmail.com>
2017-02-25 20:02:18 +0330

    Update Autoleave.lua

--- plugins/Autoleave.lua
+++ plugins/Autoleave.lua
@@ -4,11 +4,11 @@ local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
     if matches[1] == 'left' and is_sudo(msg) then
-send_large_msg(receiver, "🔄Bye everybody Hope to see you soon \nContact me in @Ev_cnt_bot", ok_cb, false)
+send_large_msg(receiver, "🔄Bye everybody Hope to see you soon \nContact me in @The_erf_Bot", ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_friend(msg) then
-       send_large_msg(receiver, "🔀If you want me to join your group \nSend me your group Link in @Ev_Cnt_Bot", ok_cb, false)
+       send_large_msg(receiver, "🔀If you want me to join your group \nSend me your group Link in @The_erf_Bot", ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     end
