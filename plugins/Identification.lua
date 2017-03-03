dd1a91fbe1cd75a3a9e784090f6c7c23b7e66fa7
Electrovirus <E.electrovirus@gmail.com>
2017-03-03 12:06:23 +0330

    Update Identification.lua

--- plugins/Identification.lua
+++ plugins/Identification.lua
@@ -1,7 +1,7 @@
 do
 function run(msg, matches)
   if matches[1] == 'ids' and is_sudo(msg) then
-return "🔄Your id : "..msg.from.id.."\n━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n🔀Group id∶"..msg.to.id..""
+return "🔄Your id : "..msg.from.id.."\n━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n🔀Group id∶"..msg.to.id..""
 end
   if matches[1] == 'phone' then
 return "🔁Your phoneNumber : +"..(msg.from.phone or '404 \nI dont have your number')..""
